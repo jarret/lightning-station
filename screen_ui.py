cccda02762be28208381ccd23d2a1e5566610dac
forkdrop <forkdrop@protonmail.com>
2019-01-01 13:45:34 -0700

    make urwid layout more sane

--- screen_ui.py
+++ screen_ui.py
@@ -75,46 +75,51 @@ class ScreenUI(object):
 
     ###########################################################################
 
-    def _wrap_filler(self, widget, title):
+    def _wrap_box(self, widget, title):
         lb = urwid.LineBox(widget, title=title)
         f = urwid.Filler(lb)
+        return urwid.AttrMap(lb, 'background')
+
+    def _wrap_filler(self, widget):
+        f = urwid.Filler(widget)
         return urwid.AttrMap(f, 'background')
+
+    ###########################################################################
 
     def _fee_estimate_widget(self):
         if 'fee_estimate' not in self.info:
-            return self._wrap_filler(urwid.Pile([]), "(no fee estimates)")
+            return self._wrap_box(urwid.Pile([]), "(no fee estimates)")
         lines = []
         for block in sorted(self.info['fee_estimate'].keys()):
             estimate = self.info['fee_estimate'][block]
             s = "%d: %0.2f sat/byte" % (block, estimate)
             lines.append(self._center_info_text(s))
-        return self._wrap_filler(urwid.Pile(lines),
-                                 "Conservative Fee Estimates")
+        return self._wrap_box(urwid.Pile(lines), "Conservative Fee Estimates")
 
     def _fee_estimate_eco_widget(self):
         if 'fee_estimate_eco' not in self.info:
-            return self._wrap_filler(urwid.Pile([]), "(no eco fee estimates)")
+            return self._wrap_box(urwid.Pile([]), "(no eco fee estimates)")
         lines = []
         for block in sorted(self.info['fee_estimate_eco'].keys()):
             estimate = self.info['fee_estimate_eco'][block]
             s = "%d: %0.2f sat/byte" % (block, estimate)
             lines.append(self._center_info_text(s))
-        return self._wrap_filler(urwid.Pile(lines), "Econmical Fee Estimates")
+        return self._wrap_box(urwid.Pile(lines), "Econmical Fee Estimates")
 
     def _mempool_widget(self):
         if 'mempool_txs' not in self.info:
-            return self._wrap_filler(urwid.Pile([]), "(no mempool data)")
+            return self._wrap_box(urwid.Pile([]), "(no mempool data)")
 
         t = self._center_info_text("txs: {:,}".format(self.info['mempool_txs']))
         b = self._center_info_text("bytes: {:,}".format(
             self.info['mempool_bytes']))
         p = self._progress_bar('used', 'free', self.info['mempool_percent'])
         lines = [t, b, p]
-        return self._wrap_filler(urwid.Pile(lines), "Mempool")
+        return self._wrap_box(urwid.Pile(lines), "Mempool")
 
     def _network_widget(self):
         if 'net_connections' not in self.info:
-            return self._wrap_filler(urwid.Pile([]), "(no network data)")
+            return self._wrap_box(urwid.Pile([]), "(no network data)")
         c = self._center_info_text("connections: %d" %
                                    self.info['net_connections'])
         v = self._center_info_text("version: %s" %
@@ -123,12 +128,12 @@ class ScreenUI(object):
         if 'ip_address' in self.info:
             i = self._center_info_text("LAN IP: %s" % self.info['ip_address'])
             lines.append(i)
-        return self._wrap_filler(urwid.Pile(lines), "Network")
+        return self._wrap_box(urwid.Pile(lines), "Network")
 
 
     def _block_widget(self):
         if 'block_name' not in self.info:
-            return self._wrap_filler(urwid.Pile([]), "(no block data)")
+            return self._wrap_box(urwid.Pile([]), "(no block data)")
         h = self._center_info_text("height: %d" %
                                     self.info['block_height'])
         bn = self._center_info_text("name: %s" %
@@ -150,18 +155,18 @@ class ScreenUI(object):
         e = self._center_info_text("elapsed: %s" % self._fmt_seconds(elapsed))
 
         lines = [h, bn, p, at, tx, s, w, t, e]
-        return self._wrap_filler(urwid.Pile(lines), "Block")
+        return self._wrap_box(urwid.Pile(lines), "Block")
 
     def _websocket_widget(self):
         if 'ws_tally' not in self.info:
-            return self._wrap_filler(urwid.Pile([]), "(no websocket data)")
+            return self._wrap_box(urwid.Pile([]), "(no websocket data)")
         t = self._center_info_text("tally: %d" % self.info['ws_tally'])
         lines = [t]
-        return self._wrap_filler(urwid.Pile(lines), "Websocket")
+        return self._wrap_box(urwid.Pile(lines), "Websocket")
 
     def _sys_res_widget(self):
         if 'mem_total' not in self.info:
-            return self._wrap_filler(urwid.Pile([]), "(no sys res data)")
+            return self._wrap_box(urwid.Pile([]), "(no sys res data)")
         t = self._center_info_text(
             "Mem Total: {:,} bytes".format(self.info['mem_total']))
         p = self._progress_bar('used', 'free', self.info['mem_used_pct'])
@@ -179,26 +184,26 @@ class ScreenUI(object):
         lines = [t, p, s, r, rd, wt, d, c]
         for cpu in self.info['cpu_pct']:
             lines.append(self._progress_bar('used', 'free', cpu))
-        return self._wrap_filler(urwid.Pile(lines), "System Resources")
+        return self._wrap_box(urwid.Pile(lines), "System Resources")
 
     def _song_playing_widget(self):
         if 'song_playing_title' not in self.info:
-            return self._wrap_filler(urwid.Pile([]), "(no song playing)")
+            return self._wrap_box(urwid.Pile([]), "(no song playing)")
         if not self.info['song_playing_title']:
-            return self._wrap_filler(urwid.Pile([]), "(no song playing)")
+            return self._wrap_box(urwid.Pile([]), "(no song playing)")
         st = self._center_info_text(
             "Title: %s" % self.info['song_playing_title'])
         sa = self._center_info_text(
             "Artist: %s" % self.info['song_playing_artist'])
         lines = [st, sa]
-        return self._wrap_filler(urwid.Pile(lines), "Now Playing")
+        return self._wrap_box(urwid.Pile(lines), "Now Playing")
 
     def _song_queue_widget(self):
         if 'queued_songs' not in self.info:
-            return self._wrap_filler(urwid.Pile([]), "(no songs queued)")
+            return self._wrap_box(urwid.Pile([]), "(no songs queued)")
         n = len(self.info['queued_songs'])
         if n == 0:
-            return self._wrap_filler(urwid.Pile([]), "(no songs queued)")
+            return self._wrap_box(urwid.Pile([]), "(no songs queued)")
 
         showing = min(n, 5)
         not_showing = (n - 5) if (n > 5) else 0
@@ -214,7 +219,7 @@ class ScreenUI(object):
         if not_showing != 0:
             m = self._center_info_text("(%d more)" % not_showing)
             lines.append(m)
-        return self._wrap_filler(urwid.Pile(lines), "Song Queue")
+        return self._wrap_box(urwid.Pile(lines), "Song Queue")
 
     ###########################################################################
 
@@ -224,15 +229,14 @@ class ScreenUI(object):
         mem = self._mempool_widget()
         net = self._network_widget()
         blk = self._block_widget()
-        ws = self._websocket_widget()
         sr = self._sys_res_widget()
+        ws = self._websocket_widget()
         sp = self._song_playing_widget()
         sq = self._song_queue_widget()
 
-        col1 = urwid.Pile([ws, fee, eco, mem])
-        col2 = urwid.Pile([net, blk, sr])
-        #col3 = urwid.AttrMap(urwid.SolidFill(), "background")
-        col3 = urwid.Pile([sp, sq])
+        col1 = self._wrap_filler(urwid.Pile([fee, eco, mem]))
+        col2 = self._wrap_filler(urwid.Pile([net, blk, sr]))
+        col3 = self._wrap_filler(urwid.Pile([ws, sp, sq]))
         cols = urwid.Columns([col1, col2, col3])
 
         self.loop.widget = cols
