f545609e05d8557b5cda5e9332a9f0fb3baf4c2b
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2019-02-23 19:13:10 +0000

    block list

--- node_info.py
+++ node_info.py
@@ -6,7 +6,7 @@ from lightningd import LightningDaemon
 
 INTERVAL = 10.0
 
-FEE_RATE_BLOCKS = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+FEE_RATE_BLOCKS = [1, 6, 12, 50, 100, 256, 512]
 
 class NodeInfo(object):
     def __init__(self, reactor, screen_ui):
