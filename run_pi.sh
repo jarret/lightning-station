f811a5f7928aad4701b37d9967c5e7114afb05c6
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2020-07-18 14:30:32 -0600

    update rpc location

--- run_pi.sh
+++ run_pi.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2019 Jarret Dyrbye
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php
-./main.py --log-file /home/pi/btc-rust/lightning-station.log /home/pi/btc-rust/audio/ /home/pi/btc-rust/lightningd-run/lightning-dir/lightning-rpc sda /home/pi/btc-rust/bitcoind-run/
+./main.py --log-file /home/pi/btc-rust/lightning-station.log /home/pi/btc-rust/audio/ /home/pi/btc-rust/lightningd-run/lightning-dir/bitcoin/lightning-rpc sda /home/pi/btc-rust/bitcoind-run/
