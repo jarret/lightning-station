245b409223696af787fdbdc84545df8677ddf748
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2020-07-18 11:43:34 -0600

    update to point to new rpc location

--- run_dev.sh
+++ run_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2019 Jarret Dyrbye
+# Copyright (c) 2020 Jarret Dyrbye
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php
-./main.py --websocket /home/jarret/audio/ /home/jarret/lightningd-run/lightning-dir/lightning-rpc sda /home/jarret/bitcoind-run/
+./main.py --websocket /home/jarret/audio/ /home/jarret/lightningd-run/lightning-dir/bitcoin/lightning-rpc sda /home/jarret/.bitcoin/
