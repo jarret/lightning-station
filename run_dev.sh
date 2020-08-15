eba59d950c863f0a5c883b2b6ca692357e40cb21
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2020-08-15 11:27:20 -0600

    point to new

--- run_dev.sh
+++ run_dev.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2020 Jarret Dyrbye
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php
-./main.py --websocket /home/jarret/audio/ /home/jarret/lightningd-run/lightning-dir/bitcoin/lightning-rpc sda /home/jarret/.bitcoin/
+./main.py --websocket /home/jarret/audio/ /home/jarret/.lightning/bitcoin/lightning-rpc sda /home/jarret/.bitcoin/
