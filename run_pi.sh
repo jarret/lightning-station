b57edd004ac552b7d4ed761a53699fa248d4876b
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2020-08-29 12:57:38 -0600

    update config

--- run_pi.sh
+++ run_pi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2019 Jarret Dyrbye
+# Copyright (c) 2020 Jarret Dyrbye
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php
-./main.py --log-file /home/pi/btc-rust/lightning-station.log /home/pi/btc-rust/audio/ /home/pi/btc-rust/lightningd-run/lightning-dir/bitcoin/lightning-rpc sda /home/pi/btc-rust/bitcoind-run/
+./main.py --log-file /home/pi/btc-rust/lightning-station.log /home/pi/btc-rust/audio/ /home/pi/.lightning/bitcoin/lightning-rpc sda /home/pi/.bitcoin/
